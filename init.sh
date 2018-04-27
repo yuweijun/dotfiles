ca7580eed1e7e39aa94c9a1a9b889d971933e821
yuweijun <yuweijun@live.com>
2018-04-27 13:56:01 +0800

    update init.sh

--- init.sh
+++ init.sh
@@ -3,19 +3,20 @@
 git submodule init
 git submodule update --remote --recursive
 
+dir=$(dirname "$0")
+
 if type apt 2>/dev/null; then
     sudo apt install -y tmux neovim mycli bash-completion
 elif type yum 2>/dev/null; then
     yum install -y tmux mycli bash-completion
 elif type brew 2>/dev/null; then
     brew install tmux neovim mycli reattach-to-user-namespace bash-completion
-    ln -sfn tmux/tmux.macosx.10.13.conf ~/.tmux.conf
 fi
 
-ln -sfn vim/ ~/.vim
+ln -sfn ${dir}/vim/ ~/.vim
 # ln -sfn vim/terminal.vimrc ~/.vimrc # with powerline font
-ln -sfn vim/server.vimrc ~/.vimrc
-ln -sfn vim/linux.gvimrc ~/.gvimrc
+ln -sfn ${dir}/vim/server.vimrc ~/.vimrc
+ln -sfn ${dir}/vim/linux.gvimrc ~/.gvimrc
 mkdir vim/bundle
 mkdir -p vim/tmp/backup vim/tmp/swap vim/tmp/undo
 mkdir -p ~/.local/share/nvim/tmp/backup ~/.local/share/nvim/tmp/swap ~/.local/share/nvim/tmp/undo
@@ -24,17 +25,17 @@ vim +PluginInstall +qall
 # brew install tmux
 # sudo apt install -y tmux
 # yum install -y tmux
-ln -sfn tmux/tmux.linux.conf ~/.tmux.conf
+ln -sfn ${dir}/tmux/tmux.linux.conf ~/.tmux.conf
 
 mkdir -p ~/.mysql/out
-ln -sfn mycli/myclirc ~/.myclirc
-ln -sfn mycli/my.cnf ~/.my.cnf
-ln -sfn mycli/my.vim ~/.my.vim
+ln -sfn ${dir}/mycli/myclirc ~/.myclirc
+ln -sfn ${dir}/mycli/my.cnf ~/.my.cnf
+ln -sfn ${dir}/mycli/my.vim ~/.my.vim
 
-ln -sfn fzf ~/.fzf
+ln -sfn ${dir}/fzf ~/.fzf
 ~/.fzf/install
 
-ln -sfn bash-completion/bash_completion ~/.bash_completion
+ln -sfn ${dir}/bash-completion/bash_completion ~/.bash_completion
 
 cat bashrc >> ~/.bashrc
 
