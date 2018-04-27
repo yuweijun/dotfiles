d64a94636e775f2cc850a2d9b29c9b2b64f0dc1b
yuweijun <yuweijun@live.com>
2018-04-27 15:34:03 +0800

    update init.sh

--- init.sh
+++ init.sh
@@ -15,15 +15,6 @@ elif type brew 2>/dev/null; then
     brew install tmux neovim mycli reattach-to-user-namespace bash-completion
 fi
 
-ln -sfn ${dir}/vim ~/.vim
-# ln -sfn vim/server.vimrc ~/.vimrc # without powerline font and using plug
-ln -sfn ${dir}/vim/terminal.vimrc ~/.vimrc # with powerline font and using vundle
-ln -sfn ${dir}/vim/linux.gvimrc ~/.gvimrc
-mkdir vim/bundle
-mkdir -p vim/tmp/backup vim/tmp/swap vim/tmp/undo
-mkdir -p ~/.local/share/nvim/tmp/backup ~/.local/share/nvim/tmp/swap ~/.local/share/nvim/tmp/undo
-vim +PluginInstall +qall
-
 # brew install tmux
 # sudo apt install -y tmux
 # yum install -y tmux
@@ -35,9 +26,31 @@ ln -sfn ${dir}/mycli/my.cnf ~/.my.cnf
 ln -sfn ${dir}/mycli/my.vim ~/.my.vim
 
 ln -sfn ${dir}/fzf ~/.fzf
-~/.fzf/install
-
 ln -sfn ${dir}/bash-completion/bash_completion ~/.bash_completion
 
-cat bashrc >> ~/.bashrc
+if type brew 2>/dev/null; then
+    if grep -q bash_completion ~/.bash_profile; then
+        echo "bash_completion config found in ~/.bash_profile"
+    else
+        cat bashrc >> ~/.bash_profile
+        ~/.fzf/install
+    fi
+else
+    if grep -q bash_completion ~/.bashrc; then
+        echo "bash_completion config found in ~/.bashrc"
+    else
+        cat bashrc >> ~/.bashrc
+        ~/.fzf/install
+    fi
+fi
+
+ln -sfn ${dir}/vim ~/.vim
+# ln -sfn vim/server.vimrc ~/.vimrc # without powerline font and using plug
+ln -sfn ${dir}/vim/terminal.vimrc ~/.vimrc # with powerline font and using vundle
+ln -sfn ${dir}/vim/linux.gvimrc ~/.gvimrc
+mkdir vim/bundle
+mkdir -p vim/tmp/backup vim/tmp/swap vim/tmp/undo
+mkdir -p ~/.local/share/nvim/tmp/backup ~/.local/share/nvim/tmp/swap ~/.local/share/nvim/tmp/undo
+# install vim plugin at last
+vim +PluginInstall +qall
 
