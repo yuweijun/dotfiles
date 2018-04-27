5be52dde53b8a88c2a618c3ace5a3122056f0e11
yuweijun <yuweijun@live.com>
2018-04-27 17:10:23 +0800

    update init.sh

--- init.sh
+++ init.sh
@@ -7,11 +7,11 @@ cd ${directory}
 dir="$(pwd)"
 
 if type apt 2>/dev/null; then
-    sudo apt install -y tmux neovim mycli bash-completion
+    sudo apt install -y tmux neovim mycli
 elif type yum 2>/dev/null; then
-    yum install -y tmux mycli bash-completion
+    yum install -y tmux mycli
 elif type brew 2>/dev/null; then
-    brew install tmux neovim mycli reattach-to-user-namespace bash-completion
+    brew install tmux neovim mycli reattach-to-user-namespace
 fi
 
 # brew install tmux
@@ -27,31 +27,22 @@ ln -sfn ${dir}/mycli/my.vim ~/.my.vim
 ln -sfn ${dir}/fzf ~/.fzf
 ln -sfn ${dir}/bash-completion/bash_completion ~/.bash_completion
 
-if type brew 2>/dev/null; then
-    if grep -q bash_completion ~/.bash_profile; then
-        echo "bash_completion config found in ~/.bash_profile"
-    else
-        cat bashrc >> ~/.bash_profile
-        ~/.fzf/install
-    fi
+if grep -q bash_completion ~/.bashrc; then
+    echo "bash_completion config found in ~/.bashrc"
 else
-    if grep -q bash_completion ~/.bashrc; then
-        echo "bash_completion config found in ~/.bashrc"
-    else
-        cat bashrc >> ~/.bashrc
-        ~/.fzf/install
-    fi
+    cat bashrc >> ~/.bashrc
+    ~/.fzf/install
 fi
 
 ln -sfn ${dir}/vim ~/.vim
 # ln -sfn vim/server.vimrc ~/.vimrc # without powerline font and using plug
 ln -sfn ${dir}/vim/terminal.vimrc ~/.vimrc # with powerline font and using vundle
 ln -sfn ${dir}/vim/linux.gvimrc ~/.gvimrc
-mkdir vim/bundle
+mkdir -p vim/bundle
 mkdir -p vim/tmp/backup vim/tmp/swap vim/tmp/undo
 mkdir -p ~/.local/share/nvim/tmp/backup ~/.local/share/nvim/tmp/swap ~/.local/share/nvim/tmp/undo
 git clone --depth=1 https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 ln -s ~/.vim/bundle/Vundle.vim ~/.vim/bundle/vundle
 # install vim plugin at last
-vim +PluginInstall +qall
+vi +PluginInstall +qall
 
