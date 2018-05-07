149e97a180bbe7f0f2949c999ef997d857ec8597
yuweijun <yuweijun@live.com>
2018-05-07 16:38:37 +0800

    update centos6/7.sh

--- os/centos6.sh
+++ os/centos6.sh
@@ -2,5 +2,5 @@
 
 cd $(dirname "$0")
 
-yum install -y http://opensource.wandisco.com/centos/6/git/x86_64/wandisco-git-release-6-1.noarch.rpm
+yum install http://opensource.wandisco.com/centos/6/git/x86_64/wandisco-git-release-6-1.noarch.rpm
 ./centos.sh
