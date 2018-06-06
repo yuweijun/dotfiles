0597a1247f86f4e313b3000d97d27534e7a98969
yuweijun <yuweijun@live.com>
2018-06-06 21:12:43 +0800

    update homebrew

--- config/homebrew.sh
+++ config/homebrew.sh
@@ -3,6 +3,6 @@
 if type brew 2> /dev/null; then
     if ! grep -q HOMEBREW_NO_AUTO_UPDATE ${RCFILE} 2> /dev/null; then
         echo "" >> ${RCFILE}
-        echo "[ \$((\$(date "+%S") % 5)) -eq 0 ] && export HOMEBREW_NO_AUTO_UPDATE=1" >> ${RCFILE}
+        echo "[ \$((\$(date \"+%s\") % 5)) -eq 0 ] && export HOMEBREW_NO_AUTO_UPDATE=1" >> ${RCFILE}
     fi
 fi
