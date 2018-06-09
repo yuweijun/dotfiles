1eb3aaf192d176e0d2842e5319b95a80a4604f59
yuweijun <yuweijun@live.com>
2018-06-09 11:36:22 +0800

    update fzf install config

--- config/fzf.sh
+++ config/fzf.sh
@@ -4,12 +4,3 @@ if [ ! -e $HOME/.fzf ]; then
     ln -sfn ${DIR}/fzf $HOME/.fzf
 fi
 
-if grep -q "/.fzf." ${RCFILE} 2> /dev/null; then
-    echo "fzf config exists"
-elif ! $SIMPLE; then
-    if [[ "$SHELL" = "/bin/zsh" ]]; then
-        $HOME/.fzf/install --no-bash --no-zsh
-    else
-        $HOME/.fzf/install --no-bash --no-zsh
-    fi
-fi
