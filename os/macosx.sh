4ccc2d0c1558696c7aa3fbca307f5546180c4006
yuweijun <yuweijun@live.com>
2018-06-11 19:35:27 +0800

    update os/macosx.sh

--- os/macosx.sh
+++ os/macosx.sh
@@ -8,25 +8,29 @@ else
     /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 fi
 
-brew install macvim nginx tmux neovim tldr shellcheck fzf cloc axel rougify mycli
+brew install coreutils findutils gnu-tar gnu-sed gawk gnutls gnu-indent gnu-getopt
+brew install nginx tmux neovim tldr shellcheck fzf cloc axel rougify mycli
 brew install wget zsh-syntax-highlighting autojump coreutils the_silver_searcher
-brew install git git-flow keepassx gnu-sed proxychains-ng
+brew install git git-flow keepassx proxychains-ng pstree
 brew install mosh bash-completion zsh-autosuggestions
 
 brew tap caskroom/versions
 
-brew install caskroom/cask/iterm2
-brew install caskroom/cask/visual-studio-code
-
-brew cask install java6
-brew cask install java7
-brew cask install java8
-brew cask install java9
+brew cask install macvim
+brew cask install iterm2
+brew cask install visual-studio-code
 brew cask install font-fira-code
 
 brew install gradle maven
+brew install nethogs nload
 brew install nmap tcpflow
 brew install wireshark --with-qt
 
 pip install virtualenv
 
+if ! type java &>/dev/null; then
+    brew cask install java6
+    brew cask install java7
+    brew cask install java8
+    brew cask install java9
+fi
