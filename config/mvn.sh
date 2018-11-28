62eb8477060fa73c51a0c613e991fc86fc53c496
yuweijun <yuweijun@live.com>
2018-11-28 12:26:32 +0800

    update config/mvn.sh

--- config/mvn.sh
+++ config/mvn.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
+JAVA_HOME=""
+
 if [ -e /usr/libexec/java_home ]; then
     JAVA_HOME=$(/usr/libexec/java_home -v 1.8)
 else
-    JAVA_HOME=$(dirname $(dirname $(readlink -f $(which javac))))
+    if [ type javac 2> /dev/null ]; then
+        JAVA_HOME=$(dirname $(dirname $(readlink -f $(which javac))))
+    fi
 fi
 
 echo -e "export JAVA_HOME=${JAVA_HOME}" > ${HOME}/.mavenrc
