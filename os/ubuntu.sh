82b35eadb68542082ab5fc908a55fc9bff6a2d73
yuweijun <yuweijun@live.com>
2018-11-28 09:01:44 +0800

    update ubuntu.sh

--- os/ubuntu.sh
+++ os/ubuntu.sh
@@ -2,7 +2,10 @@
 
 apt install -y tmux mycli silversearcher-ag mosh multitail
 
-mkdir -p ~/.local/share/fonts
-cd ~/.local/share/fonts
+SHARE_FONTS=~/.local/share/fonts
+mkdir -p $SHARE_FONTS
+cd $SHARE_FONTS
 curl -fLo "Droid Sans Mono for Powerline Nerd Font Complete.otf" https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/DroidSansMono/complete/Droid%20Sans%20Mono%20Nerd%20Font%20Complete.otf
+
+fc-cache -vf $SHARE_FONTS
 
