faed7be482d8ed281269aa48c43219627fb959b6
yuweijun <yuweijun@live.com>
2018-11-29 09:48:22 +0800

    update nerd font for gvim

--- os/ubuntu.sh
+++ os/ubuntu.sh
@@ -5,8 +5,7 @@ apt install -y tmux mycli silversearcher-ag mosh multitail
 SHARE_FONTS=~/.local/share/fonts
 mkdir -p $SHARE_FONTS
 cd $SHARE_FONTS
-curl -fLo "Droid Sans Mono for Powerline Nerd Font Complete.otf" https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/DroidSansMono/complete/Droid%20Sans%20Mono%20Nerd%20Font%20Complete.otf
-curl -fLo "Meslo LG S DZ Regular Nerd Font Complete Mono.otf" https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/Meslo/S-DZ/complete/Meslo%20LG%20S%20DZ%20Regular%20Nerd%20Font%20Complete%20Mono.otf
+curl -fLo "Meslo LG M DZ Regular Nerd Font Complete Mono.otf" https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/Meslo/M-DZ/complete/Meslo%20LG%20S%20DZ%20Regular%20Nerd%20Font%20Complete%20Mono.otf
 
 fc-cache -vf $SHARE_FONTS
 
