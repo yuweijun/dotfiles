93fc4fa910b465e9cc0d1bed8f772c0461c994fd
yuweijun <yuweijun@live.com>
2018-12-04 23:55:56 +0800

    FIX sed on Mac OSX: RE error: illegal byte sequence

--- config/sed.sh
+++ config/sed.sh
@@ -2,7 +2,7 @@
 
 if [[ $OSTYPE =~ ^darwin ]]; then
     echo "" >> ${RCFILE}
-    echo "# FIX sed on Mac OSX: RE error: illegal byte sequence" >> ${RCFILE}
-    echo 'alias sed="LC_CTYPE=C sed"' >> ${RCFILE}
-    echo "" >> ${RCFILE}
+
+    # FIX sed on Mac OSX: RE error: illegal byte sequence
+    sed -i '' -E $'s#source "\$BASH_IT"/bash_it.sh#alias sed="LC_CTYPE=C sed"\\\n\\\nsource "\$BASH_IT"/bash_it.sh#' ${RCFILE}
 fi
