9763fc3a9f8d21fb9c1709f6ed080e62a1c68305
yuweijun <yuweijun@live.com>
2019-10-17 21:42:03 +0800

    update os/macosx.sh

--- os/macosx.sh
+++ os/macosx.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-set -x
+set -ex
 
 if type brew >/dev/null 2>&1; then
     echo "brew installed"
@@ -13,32 +13,16 @@ brew install nginx tmux neovim tldr shellcheck fzf cloc axel mycli
 brew install wget zsh-syntax-highlighting autojump coreutils the_silver_searcher
 brew install git git-flow keepassx proxychains-ng pstree
 brew install mosh bash-completion zsh-autosuggestions multitail
-brew install httpie
+brew install httpie macvim
 
 brew tap caskroom/versions
 
 brew cask install google-chrome
 brew cask install macvim
 brew cask install iterm2
-brew cask install visual-studio-code
-brew cask install font-fira-code
 
-brew install gradle maven
-brew install nethogs nload
-brew install nmap tcpflow
-brew install wireshark --with-qt
+brew install groovy gradle maven
+brew install nethogs nmap tcpflow
 
 pip install virtualenv
 
-cd $HOME/Library/Fonts
-MESLO_FONT="Meslo LG M Regular Nerd Font Complete Mono.otf"
-if [ -f $MESLO_FONT ]; then
-    echo "$MESLO_FONT exists"
-else
-    curl -fLo $MESLO_FONT https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/Meslo/M/complete/Meslo%20LG%20M%20Regular%20Nerd%20Font%20Complete%20Mono.otf
-    open $MESLO_FONT
-fi
-
-if ! type java &>/dev/null; then
-    brew cask install java8
-fi
