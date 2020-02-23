10f90387d4c9cc6841106465fdd2db107b9fc305
yuweijun <yuweijun@live.com>
2020-02-23 09:05:11 +0800

    update lt more

--- config/gnu-ls.sh
+++ config/gnu-ls.sh
@@ -5,6 +5,7 @@ if type gls > /dev/null 2>&1; then
         echo "alias lg exists"
     else
         echo "alias ls='gls --color=auto'" >> ${RCFILE}
+        echo "alias lt='ls -lat | more'" >> ${RCFILE}
         echo "alias lg='/bin/ls -laG'" >> ${RCFILE}
     fi
 
