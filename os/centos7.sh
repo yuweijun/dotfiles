cceeb9af64cfccc423b2f9c6e5c01d18871c670c
Weijun Yu <weijun.yu@sap.com>
2020-02-25 09:48:54 +0800

    update os/centos7.sh

--- os/centos7.sh
+++ os/centos7.sh
@@ -2,5 +2,5 @@
 
 cd $(dirname "$0")
 
-yum install http://opensource.wandisco.com/centos/7/git/x86_64/wandisco-git-release-7-2.noarch.rpm
+yum install -y http://opensource.wandisco.com/centos/7/git/x86_64/wandisco-git-release-7-2.noarch.rpm
 ./centos.sh
