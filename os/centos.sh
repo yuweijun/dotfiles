b38623a8e8e65738f44b68c53348a5d7ed141412
Weijun Yu <weijun.yu@sap.com>
2020-02-25 10:04:29 +0800

    update os/centos.sh

--- os/centos.sh
+++ os/centos.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-yum update  -y nss curl libcurl wget zsh
-yum install -y git
+yum update  -y nss curl libcurl
+yum install -y git wget zsh
 yum install -y tmux mycli the_silver_searcher mosh multitail
 
 if type pip > /dev/null 2>&1; then
