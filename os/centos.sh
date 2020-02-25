9cfe2af1d6852a2770d50eae9212fa162550ea4d
Weijun Yu <weijun.yu@sap.com>
2020-02-25 13:05:16 +0800

    update os/centos.sh

--- os/centos.sh
+++ os/centos.sh
@@ -3,6 +3,7 @@
 yum update  -y nss curl libcurl
 yum install -y git wget zsh
 yum install -y vim tmux
+yum install -y nmap-ncat bash-completion
 
 if type pip > /dev/null 2>&1; then
     pip install virtualenv
