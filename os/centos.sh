ba383d00dd54ebd28ccc212f1ae26cf9d28bdf2b
Weijun Yu <weijun.yu@sap.com>
2020-02-25 09:47:07 +0800

    update os/centos.sh

--- os/centos.sh
+++ os/centos.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-yum update  -y nss  curl  libcurl
+yum update  -y nss curl libcurl wget zsh
 yum install -y git
 yum install -y tmux mycli the_silver_searcher mosh multitail
 
-pip install virtualenv
+if type pip > /dev/null 2>&1; then
+    pip install virtualenv
+fi
+
