28bb396de5ec928c5f138768794f40c56958e435
yuweijun <yuweijun@live.com>
2020-02-26 21:30:10 +0800

    export CLICOLOR for MacOS ls

--- bash-custom/plugins/env.bash
+++ bash-custom/plugins/env.bash
@@ -5,20 +5,9 @@ if [ -f $HOME/.envrc ]; then
 fi
 
 export LANG=en_US.UTF-8
+export CLICOLOR=1
+export LSCOLORS=GxFxCxDxBxegedabagaced
+
 export VIRTUAL_ENV_DISABLE_PROMPT=1
 export PATH=$PATH:/usr/local/sbin:$HOME/bin
 
-if type gls > /dev/null 2>&1; then
-    if [ -n "$CLICOLOR" ]; then
-        echo "CLICOLOR config exists"
-    else
-        echo "export CLICOLOR=1" >> ${RCFILE}
-    fi
-
-    if [ -n "$LSCOLORS" ]; then
-        echo "LSCOLORS config exists"
-    else
-        echo "" >> ${RCFILE}
-        echo "export LSCOLORS=GxFxCxDxBxegedabagaced" >> ${RCFILE}
-    fi
-fi
--- zsh-custom/plugins/env/env.plugin.zsh
+++ zsh-custom/plugins/env/env.plugin.zsh
@@ -1,27 +1,15 @@
-#!/bin/zsh
+# env setting for zsh
 
 if [ -f $HOME/.envrc ]; then
     source $HOME/.envrc
 fi
 
 export LANG=en_US.UTF-8
+export CLICOLOR=1
+export LSCOLORS=GxFxCxDxBxegedabagaced
+
 export VIRTUAL_ENV_DISABLE_PROMPT=1
 export PATH=$PATH:/usr/local/sbin:$HOME/bin
-
-if type gls > /dev/null 2>&1; then
-    if [ -n "$CLICOLOR" ]; then
-        echo "CLICOLOR config exists"
-    else
-        echo "export CLICOLOR=1" >> ${RCFILE}
-    fi
-
-    if [ -n "$LSCOLORS" ]; then
-        echo "LSCOLORS config exists"
-    else
-        echo "" >> ${RCFILE}
-        echo "export LSCOLORS=GxFxCxDxBxegedabagaced" >> ${RCFILE}
-    fi
-fi
 
 unsetopt BEEP
 
