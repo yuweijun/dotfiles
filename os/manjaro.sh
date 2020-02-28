99a6c10fd8b001e89bc7e591106245c4805f6e16
Weijun Yu <weijun.yu@sap.com>
2020-02-28 13:07:21 +0800

    add os/manjaro.sh

---
+++ os/manjaro.sh
@@ -0,0 +1,8 @@
+sudo pacman -S --noconfirm neovim git
+sudo pacman -S --noconfirm bat ripgrep fd exa jq
+sudo pacman -S --noconfirm xclip xsel
+sudo pacman -S --noconfirm binutils make gcc fakeroot expac yajl
+sudo pacman -S --noconfirm docker
+sudo pacman -S --noconfirm ethtool wget unzip
+sudo pacman -S --noconfirm pamac-common
+sudo pacman -S --noconfirm go
