3a216f394d53841f006b31dc56cca215e98985fc
Weijun Yu <weijun.yu@sap.com>
2020-02-29 08:45:58 +0800

    update env plugin

--- bash-custom/plugins/env.bash
+++ bash-custom/plugins/env.bash
@@ -1,13 +1,16 @@
 #!/bin/bash
 
-if [ -f $HOME/.envrc ]; then
-    source $HOME/.envrc
-fi
-
 export LANG=en_US.UTF-8
-export CLICOLOR=1
-export LSCOLORS=GxFxCxDxBxegedabagaced
 
 export VIRTUAL_ENV_DISABLE_PROMPT=1
 export PATH=$PATH:/usr/local/sbin:$HOME/bin
+
+if [[ $OSTYPE =~ ^darwin ]]; then
+    export CLICOLOR=1
+    export LSCOLORS=GxFxCxDxBxegedabagaced
+fi
+
+if [ -f $HOME/.envrc ]; then
+    source $HOME/.envrc
+fi
 
--- zsh-custom/plugins/env/env.plugin.zsh
+++ zsh-custom/plugins/env/env.plugin.zsh
@@ -1,15 +1,18 @@
 # env setting for zsh
 
-if [ -f $HOME/.envrc ]; then
-    source $HOME/.envrc
-fi
-
 export LANG=en_US.UTF-8
-export CLICOLOR=1
-export LSCOLORS=GxFxCxDxBxegedabagaced
 
 export VIRTUAL_ENV_DISABLE_PROMPT=1
 export PATH=$PATH:/usr/local/sbin:$HOME/bin
 
 unsetopt BEEP
+
+if [[ $OSTYPE =~ ^darwin ]]; then
+    export CLICOLOR=1
+    export LSCOLORS=GxFxCxDxBxegedabagaced
+fi
+
+if [ -f $HOME/.envrc ]; then
+    source $HOME/.envrc
+fi
 
