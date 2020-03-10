28990cc0779fb707e8318b7137a6d47e2e0e7bbd
Weijun Yu <weijun.yu@sap.com>
2020-03-10 16:00:11 +0800

    update install.sh

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ cd "$(dirname $0)"
 export DIR="$(pwd)"
 
 git submodule update --init --recursive
-git submodule foreach git checkout master
+git submodule foreach git checkout
 
 export RCFILE="${HOME}/.bashrc"
 if [ "${SHELL}" = "/bin/zsh" ]; then
