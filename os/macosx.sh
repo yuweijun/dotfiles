d41f7a5ef677afdac85e5945d21b4d7231917a0a
Weijun Yu <weijun.yu@sap.com>
2020-03-12 12:18:40 +0800

    update macosx.sh adding more fonts

--- os/macosx.sh
+++ os/macosx.sh
@@ -6,6 +6,17 @@ else
     /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 fi
 
+# install fonts from https://github.com/Homebrew/homebrew-cask-fonts/tree/master/Casks
 brew tap "homebrew/cask-fonts"
 brew cask install font-hack-nerd-font
+brew cask install font-dejavu-sans-mono-for-powerline
+brew cask install font-menlo-for-powerline
+brew cask install font-meslo-for-powerline
+brew cask install font-source-code-pro-for-powerline
+brew cask install font-meslo-nerd-font-mono
+brew cask install font-droid-sans-mono-for-powerline
+brew cask install font-fira-mono-for-powerline
+brew cask install font-fira-code
+brew cask install font-firacode-nerd-font-mono
 
+brew cask install font-fontawesome font-awesome-terminal-fonts
