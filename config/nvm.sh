a887164d9651aafffb667e3ee531697284c07cc7
Weijun Yu <weijun.yu@sap.com>
2020-03-13 17:35:07 +0800

    add brew aha and update nvm

--- config/nvm.sh
+++ config/nvm.sh
@@ -2,6 +2,7 @@
 
 if [ ! -e $HOME/.nvm ]; then
     cd ${HOME}
-    curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
+    # curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
+    curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
 fi
 
