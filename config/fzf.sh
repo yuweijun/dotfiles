00f079a40b9cac29d2892b01e512f2a79685ddd1
yuweijun <yuweijun@live.com>
2020-03-14 15:37:06 +0800

    update config/fzf.sh

--- config/fzf.sh
+++ config/fzf.sh
@@ -4,6 +4,6 @@ if [ ! -e $HOME/.fzf ]; then
     DIR=${DIR:-$(pwd)}
 
     ln -sfn ${DIR}/fzf $HOME/.fzf
-    ${HOME}/.fzf/install --no-bash --no-fish --no-zsh
+    # ${HOME}/.fzf/install --no-bash --no-fish --no-zsh
 fi
 
