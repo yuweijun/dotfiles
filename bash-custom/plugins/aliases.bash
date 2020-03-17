236ced578a5e58f6c9dd56131533d9ef9a68e70b
Weijun Yu <weijun.yu@sap.com>
2020-03-17 21:43:09 +0800

    update aliases plugin

--- bash-custom/plugins/aliases.bash
+++ bash-custom/plugins/aliases.bash
@@ -6,33 +6,29 @@ alias ld='ls -ad'
 alias la='ls -a'
 alias l='ls -a'
 
-if type nvim &> /dev/null; then
+if type nvim &>/dev/null; then
     alias vim="nvim"
 fi
 
 alias vi="vim"
 alias vzrc='vi ~/.zshrc'
 
-if type git &> /dev/null; then
-    alias master="git checkout master"
-fi
-
-if type gls &> /dev/null; then
+if type gls &>/dev/null; then
     # gnu-ls command for Mac OS
     alias ls="gls --color=auto"
-elif type colorls &> /dev/null; then
+elif type colorls &>/dev/null; then
     # sudo gem install colorls
     alias ls="colorls"
-elif type exa &> /dev/null; then
+elif type exa &>/dev/null; then
     alias ls="exa"
     alias lt="exa -la -t modified"
 fi
 
-if type bat &> /dev/null; then
+if type bat &>/dev/null; then
     alias cat="bat -p"
 fi
 
-if type systemctl &> /dev/null; then
+if type systemctl &>/dev/null; then
     alias sc-running='systemctl list-units --type=service --state=running'
 fi
 
