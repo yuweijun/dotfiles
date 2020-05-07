ee9b65ffb69e75ad101dc6e5a086493ea7d943a3
Weijun Yu <weijun.yu@sap.com>
2020-05-07 11:14:27 +0800

    update nvim install

--- config/nvim.sh
+++ config/nvim.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
 
-if [ ! -e ${HOME}/.config/nvim ]; then
-    mkdir -p ${HOME}/.config/nvim
-    mkdir -p ${HOME}/.local/share/nvim/tmp/backup ${HOME}/.local/share/nvim/tmp/swap ${HOME}/.local/share/nvim/tmp/undo
+if [ ! -e "${HOME}/.config/nvim" ]; then
+    mkdir -p "${HOME}/.config/nvim"
+    mkdir -p "${HOME}/.local/share/nvim/site"
+    mkdir -p "${HOME}/.local/share/nvim/tmp/backup" "${HOME}/.local/share/nvim/tmp/swap" "${HOME}/.local/share/nvim/tmp/undo"
 fi
 
-if [ ! -e ${HOME}/.vim/tmp/plug.vim ]; then
+if [ ! -e "${HOME}/.vim/tmp/plug.vim" ]; then
     DIR=${DIR:-$(pwd)}
-    if [ ! -e $HOME/.vim ]; then
-        ln -sfn ${DIR}/vim $HOME/.vim
+    if [ ! -e "${HOME}/.vim" ]; then
+        ln -sfn "${DIR}/vim" "${HOME}/.vim"
     fi
 
-    ln -sfn ${DIR}/vim/nvim.init.vim ${HOME}/.config/nvim/init.vim
-    curl -fLo ${HOME}/.vim/tmp/plug.vim https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+    ln -sfn "${DIR}/vim/nvim.init.vim" "${HOME}/.config/nvim/init.vim"
+    ln -sfn "${DIR}/vim/pack" "${HOME}/.local/share/nvim/site/pack"
+    curl -fLo "${HOME}/.vim/tmp/plug.vim" https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 fi
 
 if type nvim &> /dev/null; then
