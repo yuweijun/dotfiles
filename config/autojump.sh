12c96c375cf789dffe0cffc297bec8ed882899f6
Weijun Yu <weijun.yu@sap.com>
2020-05-09 10:11:21 +0800

    update autojump

--- config/autojump.sh
+++ config/autojump.sh
@@ -3,9 +3,8 @@
 if [ ! -e "$HOME/.autojump" ]; then
     DIR=${DIR:-$(pwd)}
 
-    if type python 2&>/dev/null; then
+    if type python &>/dev/null; then
         cd "${DIR}/autojump" || return
-        export SHELL=/bin/bash
         ./install.py
         cd - || return
     fi
